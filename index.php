7f75a772cfbc35e376810b57f070be4f2f0b861f
mackliu <cgk0910@gmail.com>
2024-08-05 15:25:08 +0800

    整理圖形素材

--- index.php
+++ index.php
@@ -26,7 +26,6 @@
                                 <a href="?do=admin">管理登入</a>
                         </div>
                         <marquee>
-
                                 情人節特惠活動 &nbsp; 為了慶祝七夕情人節，將舉辦情人兩人到現場有七七折之特惠活動~
                         </marquee>
                 </div>
@@ -35,8 +34,7 @@
                         </div>
                         <span>
                                 <div>進站總人數</div>
-                                <div style="color:#f00; font-size:28px;">
-                                        00005 </div>
+                                <div style="color:#f00; font-size:28px;"> 00005 </div>
                         </span>
                 </div>
                 <div id="right">
