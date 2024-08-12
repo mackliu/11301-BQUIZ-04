148d1ca132ea4708467a070b94dce5dcd51e46cd
mackliu <macklun@gmail.com>
2024-08-12 11:23:30 +0800

    透過網址的type參數來取得對應的商品資料

--- front/main.php
+++ front/main.php
@@ -1,1 +1,19 @@
-<button onclick="location.href='?do=buycart'">我要購買</button>
+<?php
+
+$type = $_GET['type'] ?? 0;
+
+if ($type == 0) {
+    $rows = $Goods->all(['sh' => 1]);
+} else {
+    $tmp = $Type->find($type);
+    if ($tmp['big_id'] == 0) {
+        //大分類
+        $rows = $Goods->all(['sh' => 1, 'big' => $type]);
+    } else {
+        //中分類
+        $rows = $Goods->all(['sh' => 1, 'mid' => $type]);
+    }
+}
+
+
+//dd($rows);
--- index.php
+++ index.php
@@ -49,23 +49,21 @@ include_once "./api/base.php";
         <div id="left" class="ct">
             <div style="min-height:400px;">
                 <div class="ww">
-                    <a href="">全部商品</a>
+                    <a href="?type=0">全部商品</a>
                 </div>
                 <?php
                 $bigs = $Type->all(['big_id' => 0]);
                 foreach ($bigs as $big) {
                     echo "<div class='ww'>";
-                    echo "<a href=''>{$big['name']}</a>";
+                    echo "<a href='?type={$big['id']}'>{$big['name']}</a>";
                     if ($Type->count(['big_id' => $big['id']]) > 0) {
                         $mids = $Type->all(['big_id' => $big['id']]);
                         echo "<div class='s'>";
                         foreach ($mids as $mid) {
-                            echo "<a href=''>{$mid['name']}</a>";
+                            echo "<a href='?type={$mid['id']}'>{$mid['name']}</a>";
                         }
                         echo "</div>";
                     }
-
-
                     echo "</div>";
                 }
 
@@ -77,7 +75,6 @@ include_once "./api/base.php";
             </span>
         </div>
         <div id="right">
-            <a href="">全部商品</a>
             <?php
             $do = $_GET['do'] ?? 'main';
             $file = "front/$do.php";
