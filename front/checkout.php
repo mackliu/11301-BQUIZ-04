d73513483dbe5757aa69a7110a85b8ea77aa1cce
mackliu <macklun@gmail.com>
2024-08-13 09:11:38 +0800

    建立結帳畫面

---
+++ front/checkout.php
@@ -0,0 +1,42 @@
+<h2 class="ct">填寫資料</h2>
+
+<table class="all">
+    <tr>
+        <td class="tt ct">登入帳號</td>
+        <td class="pp"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">姓名</td>
+        <td class="pp"><input type="text" name="name" id="name" value=""></td>
+    </tr>
+    <tr>
+        <td class="tt ct">電子信箱</td>
+        <td class="pp"><input type="text" name="email" id="email" value=""></td>
+    </tr>
+    <tr>
+        <td class="tt ct">聯絡地址</td>
+        <td class="pp"><input type="text" name="addr" id="addr" value=""></td>
+    </tr>
+    <tr>
+        <td class="tt ct">聯絡電話</td>
+        <td class="pp"><input type="text" name="tel" id="tel" value=""></td>
+    </tr>
+</table>
+<!-- table.all.ct>(tr.tt>td*5)+(tr.pp>td*5) -->
+<table class="all ct">
+    <tr class="tt">
+        <td>商品名稱</td>
+        <td>編號</td>
+        <td>數量</td>
+        <td>單價</td>
+        <td>小計</td>
+    </tr>
+    <tr class="pp">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+<div class="tt ct all">總價:</div>
